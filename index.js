30ad113d407fca0e326bab59da40a16fb3e0aa58
Mike Fleming <flemingatx@gmail.com>
2017-08-26 17:15:06 -0500

    adds logging

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
+  console.log('~~~~~~~~~~~~~~ RECIEVED REQUEST: ', req.body);
   res.set('Content-Type', 'text/plain');
   res.status(200).send(req.body.challenge);
-  console.log('~~~~~~~~~~~~~~ RECIEVED REQUEST: ', req.body);
 });
 
 app.listen(8080, function () {
