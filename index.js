98ea69e242911b1dcb45478d9054e1825abae273
Mike Fleming <flemingatx@gmail.com>
2017-08-27 10:35:35 -0500

    adds channel id

--- index.js
+++ index.js
@@ -3,14 +3,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const routes = require('./routes');
+
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/', function (req, res) {
-  res.set('Content-Type', 'text/plain');
-  res.status(200).send(req.body.challenge);
-  console.log('~~~~~~~~~~~~~~ RECIEVED REQUEST: ', req.body);
-});
+app.use('/', routes);
 
 app.listen(8080, function () {
   console.log('Spot Dawg is on port 8080!')
--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ routes.post('/', function (req, res) {
   res.set('Content-Type', 'text/plain');
   res.status(200).send(req.body.challenge);
   console.log('~~~~~~~~~~~~~~ RECIEVED REQUEST: ', req.body);
+  console.log(`is from target channel? ${process.env.CHANNEL_ID === req.body.event.channel}`)
 });
 
 module.exports = routes;
