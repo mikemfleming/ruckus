c5d73d2bd39e0fdb4e1a07134e839258bbd91825
Mike Fleming <flemingatx@gmail.com>
2017-08-27 10:47:16 -0500

    logs event

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ routes.post('/', function (req, res) {
   res.set('Content-Type', 'text/plain');
   res.status(200).send(req.body.challenge);
   console.log('~~~~~~~~~~~~~~ RECIEVED REQUEST: ', req.body);
+  console.log('event: ', req.body.event);
   console.log(`is from target channel? ${process.env.CHANNEL_ID === req.body.event.channel}`)
 });
 
