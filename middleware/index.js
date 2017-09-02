ccdbdc83f3161d1179bd4e6d72986a6dfadabbc5
Mike Fleming <flemingatx@gmail.com>
2017-09-02 13:54:05 -0500

    robustens the spy

--- middleware/index.js
+++ middleware/index.js
@@ -4,14 +4,15 @@ const middleware = {};
 
 middleware.spy = function(req, res, next) {
   console.log('begin ~~~~~~~~~~~~~~~~~~');
-  console.log(`RECIEVED ${req.method} REQUEST`);
+  console.log('end ~~~~~~~~~~~~~~~~~~~~');
+  console.log(`>>> RECIEVED ${req.method} REQUEST TO ${req.url}`);
   if (Object.keys(req.body).length) {
     console.log(`BODY: ${JSON.stringify(req.body, null, 2)}`);
   }
   if (Object.keys(req.query).length) {
     console.log(`QUERY: ${JSON.stringify(req.query, null, 2)}`);
   }
-  console.log('end ~~~~~~~~~~~~~~~~~~');
+  console.log('end ~~~~~~~~~~~~~~~~~~~~');
   next();
 };
 
