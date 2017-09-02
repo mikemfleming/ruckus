879a438682c2525ffe185600db85f6507ab242c5
Mike Fleming <flemingatx@gmail.com>
2017-09-02 14:25:45 -0500

    formats logs

--- middleware/index.js
+++ middleware/index.js
@@ -4,16 +4,15 @@ const middleware = {};
 
 middleware.spy = function(req, res, next) {
   console.log('begin ~~~~~~~~~~~~~~~~~~');
-  console.log('end ~~~~~~~~~~~~~~~~~~~~');
   console.log(`>>> RECIEVED ${req.method} REQUEST TO ${url.parse(req.url).pathname}`);
   if (Object.keys(req.body).length) {
-    console.log(`BODY: ${JSON.stringify(req.body, null, 2)}`);
+    console.log(`>>>> BODY: ${JSON.stringify(req.body, null, 2)}`);
   }
   if (Object.keys(req.params).length) {
-    console.log(`PARAMS: ${JSON.stringify(req.params, null, 2)}`)
+    console.log(`>>>> PARAMS: ${JSON.stringify(req.params, null, 2)}`)
   }
   if (Object.keys(req.query).length) {
-    console.log(`QUERY: ${JSON.stringify(req.query, null, 2)}`);
+    console.log(`>>>> QUERY: ${JSON.stringify(req.query, null, 2)}`);
   }
   console.log('end ~~~~~~~~~~~~~~~~~~~~');
   next();
