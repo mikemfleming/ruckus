8f2d665de3b1207f6dd28a328e399fb1c320a33c
Mike Fleming <flemingatx@gmail.com>
2017-09-11 20:25:02 -0500

    changes login url

--- server/auth/spotify/index.js
+++ server/auth/spotify/index.js
@@ -3,7 +3,7 @@
 const spotifyAuth = require('./spotify.auth');
 
 module.exports = (app) => {
-  app.get('/login', spotifyAuth.login);
+  app.get('/oauth/login', spotifyAuth.login);
   app.get('/callback', spotifyAuth.callback);
   app.get('/refresh_token', spotifyAuth.refreshToken);
 };
