f84f1e984bc0776e36f0bdd87b18395c9c6fe89c
Mike Fleming <flemingatx@gmail.com>
2017-09-13 08:26:33 -0500

    adds mauthra

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,6 +1,11 @@
 'use strict';
 
+const config = require('../../config/main.config');
+
 module.exports = (app) => {
   // Spotify routes
   require('./spotify')(app);
+
+  // Auth
+  require('mauthra')({ app, db: config.MONGO_URL });
 };
