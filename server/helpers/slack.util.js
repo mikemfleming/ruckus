61dbf20be86a5fd88cffb28f2131d0b498154dc4
Mike Fleming <flemingatx@gmail.com>
2017-09-18 17:42:53 -0500

    checking bot integration

--- server/helpers/slack.util.js
+++ server/helpers/slack.util.js
@@ -4,6 +4,7 @@ const slack = {};
 
 slack.challenge = function(req, res, next) {
   if (req.body.challenge) {
+    console.log('responding to challenge')
     res.set('Content-Type', 'text/plain');
     res.status(200).send(req.body.challenge);
   }
