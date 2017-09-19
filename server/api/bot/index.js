b3e445bda7d3f3bd320968a3bc452420c525159c
Mike Fleming <flemingatx@gmail.com>
2017-09-19 15:58:41 -0500

    cleans up

--- server/api/bot/index.js
+++ server/api/bot/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const tracks = require('../spotify/tracks.controller');
+
 module.exports = (req, res) => {
   const messageText = req.body.event.text;
   const spotifyTrack = /https:\/\/open.spotify.com\/track\/(.{22})>?/.exec(messageText);
 
   if (messageText && spotifyTrack) {
-    console.log('someone shared a spotify link', spotifyTrack[1]);
+    const trackId = spotifyTrack[1];
+    tracks.add(trackId);
   }
 
   res.end();
