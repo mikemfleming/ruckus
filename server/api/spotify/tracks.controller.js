928eda40c22372e1d8fe4921cf9fe19d79577a95
Mike Fleming <flemingatx@gmail.com>
2017-09-19 15:58:55 -0500

    adds track logic

--- server/api/spotify/tracks.controller.js
+++ server/api/spotify/tracks.controller.js
@@ -1,6 +1,21 @@
 'use strict';
 
-exports.add = (req, res) => {
-  console.log('made it to ADD', req.body);
-  res.end()
+const request = require('request');
+
+exports.add = (track) => {
+  console.log('add track: ', track);
+
+  const options = {
+    url: `https://api.spotify.com/v1/users/1228406874/playlists/4qIaLCTPEef0Zsy8G4deZz/tracks?uris=spotify:track:${track}`,
+    headers: { Authorization: 'Bearer ' + process.env.mike },
+    json: true,
+
+  };
+  const callback = (err, response, body) => {
+    if (err) return console.log('error ', err);
+    console.log('~~~ response');
+    console.log('$$$$ body', body);
+  };
+
+  request.post(options, callback);
 };
