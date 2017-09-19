076c2c4d88251f5325375496a5d4be2f30f1e618
Mike Fleming <flemingatx@gmail.com>
2017-09-19 13:16:23 -0500

    cleans up

--- server/api/bot/index.js
+++ server/api/bot/index.js
@@ -1,14 +1,15 @@
 'use strict';
 
 module.exports = (req, res) => {
-  // figure out what the message says
-  const message = req.body.event.text;
-  const spotifyTrack = /https:\/\/open.spotify.com\/track\/(.{22})>?/.exec(message);
-  // if it contains some kind of trigger
-  if (spotifyTrack) {
-    console.log('spotify share', spotifyTrack[1]);
-  } else {
-    console.log('random message', message);
+  const messageText = req.body.event.text;
+  const spotifyTrack = /https:\/\/open.spotify.com\/track\/(.{22})>?/.exec(messageText);
+
+  if (messageText) {
+    if (spotifyTrack) {
+      console.log('spotify share', spotifyTrack[1]);
+    } else {
+      console.log('random message', messageText);
+    }
   }
   res.end();
 };
