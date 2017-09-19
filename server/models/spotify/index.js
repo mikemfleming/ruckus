e55cdc22528f003b2176d12cc21d4da2071dd561
Mike Fleming <flemingatx@gmail.com>
2017-09-19 16:36:51 -0500

    moves slack logic to helpers

--- server/models/spotify/index.js
+++ server/models/spotify/index.js
@@ -3,6 +3,7 @@
 const request = require('request');
 
 const config = require('../../../config/main.config');
+const slack = require('../../helpers/slack.util');
 
 exports.add = (track) => {
   console.log('add track: ', track);
@@ -15,23 +16,7 @@ exports.add = (track) => {
   };
   const callback = (err, response, body) => {
     if (err || body.error) return console.log('error ', body.error);
-
-    const options = {
-      method: 'POST',
-      uri: 'https://slack.com/api/chat.postMessage',
-      form: {
-        token: config.SLACK_BOT_TOKEN,
-        channel: 'C6U6X16TU',
-        text: 'Bork! Ruff.',
-        as_user: false,
-        username: 'spotdawg'
-      }
-    };
-
-    request.post(options, (err, response, body) => {
-      if (err) return console.log('error ', err);
-      console.log('sent response to slack');
-    });
+    slack.respond();
   };
 
   request.post(options, callback);
