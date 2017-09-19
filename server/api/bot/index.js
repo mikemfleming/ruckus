a20e9d1ae22cdd08a187789b02f9a502b36759f5
Mike Fleming <flemingatx@gmail.com>
2017-09-18 21:42:18 -0500

    parses track id

--- server/api/bot/index.js
+++ server/api/bot/index.js
@@ -3,10 +3,10 @@
 module.exports = (req, res) => {
   // figure out what the message says
   const message = req.body.event.text;
+  const spotifyTrack = /https:\/\/open.spotify.com\/track\/(.*)>/.exec(message)[1];
 
   // if it contains some kind of trigger
   if (spotifyTrack) {
-    const trackId = /https:\/\/open.spotify.com\/track\/(.*)>/.exec(message)[1];
     console.log('spotify share', trackId)
   } else {
     console.log('random message', message)
