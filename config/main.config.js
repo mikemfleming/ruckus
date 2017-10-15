1394a232eb6d21455895b77111502ef9d72a8ff7
miek <flemingatx@gmail.com>
2017-10-15 15:18:03 -0500

    adds routes to config

--- config/main.config.js
+++ config/main.config.js
@@ -3,19 +3,47 @@
 module.exports = (() => {
   const LOG_LEVEL = process.env.LOG_LEVEL || 'production';
   const PORT = process.env.PORT || 8888;
-  const SLACK_CLIENT_ID = process.env.SLACK_CLIENT_ID;
-  const SLACK_CLIENT_SECRET = process.env.SLACK_CLIENT_SECRET;
-  const SLACK_VERIFICATION_TOKEN = process.env.SLACK_VERIFICATION_TOKEN;
-  const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
-  const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-  const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
-  const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
-  const MONGO_URL = process.env.MONGO_URL;
-  const SESSION_SECRET = process.env.SESSION_SECRET;
+
+  const LOGIN_URL = '/login';
+  const SIGNUP_URL = '/signup'
+  const PROFILE_URL = '/profile';
+  const LOGOUT_URL = '/logout';
+
+  const AUTHORIZE_ROOT_URL = '/authorize';
+  const AUTHORIZE_SLACK_ROOT_URL = '/authorize/slack';
+  const AUTHORIZE_SLACK_CALLBACK_URL = '/authorize/slack/callback';
+  const AUTHORIZE_SPOTIFY_ROOT_URL = '/authorize/spotify';
+  const AUTHORIZE_SPOTIFY_CALLBACK_URL = '/authorize/spotify/callback';
+
+  const {
+    SLACK_CLIENT_ID,
+    SLACK_CLIENT_SECRET,
+    SLACK_VERIFICATION_TOKEN,
+    SLACK_BOT_TOKEN,
+    SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET,
+    SPOTIFY_REDIRECT_URI,
+    MONGO_URL,
+    SESSION_SECRET
+  } = process.env;
+
+  console.log(process.env.MONGO_URL)
 
   return {
     LOG_LEVEL,
     PORT,
+
+    LOGIN_URL,
+    SIGNUP_URL,
+    PROFILE_URL,
+    LOGOUT_URL,
+
+    AUTHORIZE_ROOT_URL,
+    AUTHORIZE_SLACK_ROOT_URL,
+    AUTHORIZE_SLACK_CALLBACK_URL,
+    AUTHORIZE_SPOTIFY_ROOT_URL,
+    AUTHORIZE_SPOTIFY_CALLBACK_URL,
+
     SLACK_CLIENT_ID,
     SLACK_CLIENT_SECRET,
     SLACK_VERIFICATION_TOKEN,
--- server/routes.js
+++ server/routes.js
@@ -6,6 +6,7 @@ const localAuth = require('./auth/local.auth');
 const slackAuth = require('./auth/slack.auth');
 const spotifyAuth = require('./auth/spotify.auth');
 const middleware = require('./middleware');
+const config = require('../config/main.config');
 
 // #addToConfig
 module.exports = function (app) {
@@ -18,19 +19,20 @@ module.exports = function (app) {
 
 	app.get('/', (req, res) => res.render('index.ejs'));
 
-	app.get('/login', localAuth.login);
-	app.post('/login', passport.authenticate('local-login', passportOptions));
+	app.get(config.LOGIN_URL, localAuth.login);
+	app.post(config.LOGIN_URL, passport.authenticate('local-login', passportOptions));
 
-	app.get('/signup', localAuth.signup);
-	app.post('/signup', passport.authenticate('local-signup', passportOptions));
+	app.get(config.SIGNUP_URL, localAuth.signup);
+	app.post(config.SIGNUP_URL, passport.authenticate('local-signup', passportOptions));
 
-	app.get('/profile', middleware.isLoggedIn, localAuth.profile);
-	app.get('/logout', localAuth.logout);
+	app.get(config.PROFILE_URL, middleware.isLoggedIn, localAuth.profile);
 
-	app.get('/authorize', middleware.isLoggedIn, localAuth.authorize);
-	app.get('/authorize/slack', middleware.isLoggedIn, slackAuth.authorize);
-	app.get('/authorize/slack/callback', middleware.isLoggedIn, slackAuth.callback);
+	app.get(config.LOGOUT_URL, localAuth.logout);
 
-	app.get('/authorize/spotify', middleware.isLoggedIn, spotifyAuth.authorize);
-	app.get('/authorize/spotify/callback', middleware.isLoggedIn, spotifyAuth.callback);
+	app.get(config.AUTHORIZE_ROOT_URL, middleware.isLoggedIn, localAuth.authorize);
+	app.get(config.AUTHORIZE_SLACK_ROOT_URL, middleware.isLoggedIn, slackAuth.authorize);
+	app.get(config.AUTHORIZE_SLACK_CALLBACK_URL, middleware.isLoggedIn, slackAuth.callback);
+
+	app.get(config.AUTHORIZE_SPOTIFY_ROOT_URL, middleware.isLoggedIn, spotifyAuth.authorize);
+	app.get(config.AUTHORIZE_SPOTIFY_CALLBACK_URL, middleware.isLoggedIn, spotifyAuth.callback);
 };
