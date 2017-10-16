5be24d46b408bfa99109097c308c1dac8d426513
miek <flemingatx@gmail.com>
2017-10-15 21:47:35 -0500

    test vars, updates .env and .sample-env

--- test/config/main.config.test.js
+++ test/config/main.config.test.js
@@ -1,9 +1,25 @@
-const assert = require('assert');
+'use strict';
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
+const assert = require('assert');
+const fs = require('fs');
+
+require('dotenv').config(); // set env variables
+const config = require('../../config/main.config');
+
+describe('Configuration and Environment', function() {
+	describe('Node version', function () {
+		it('Should be on Node v8.6.0', function () {
+			assert.equal(process.version, 'v8.6.0');
+		});
+	});
+
+	describe('Environment Variables', function() {
+		const variables = fs.readFileSync('.sample-env')
+				.toString()
+				.split('\n')
+				.map((pair) => pair.split('=')[0])
+				.filter(variable => variable !== '');
+
+		variables.forEach((name) => it(`Sets ${name}`, () => assert(!!config[name], true)));
+	});
 });
