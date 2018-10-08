0a0945ac976c6457696e9e6b870ab6005dbc2108
miek <flemingatx@gmail.com>
2018-10-07 23:13:09 -0500

    from the ashes

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 
-exports.handler = (event, context) => {
-	context.succeed('hello world!')
-}
+const qs = require('querystring')
+
+const slackbot = require('./slackbot')
+
+exports.handler = async event => {
+	let payload
+
+	try {
+		const input = qs.decode(
+			Buffer.from(event.body, 'base64').toString()
+		)
+
+		const output = slackbot(input)
+
+		payload = JSON.stringify(output)
+	} catch (error) {
+		payload = error.message
+	}
+
+	const response = {
+			statusCode: 200,
+			body: payload
+	}
+
+	return response
+};
